3aa70f4652e7b3793055afb5db213f8442666d20
Martin Stoffel <martin.adam.stoffel@gmail.com>
2022-06-21 14:58:43 +0100

    fixed trait combinations

--- transmission_fidelity.py
+++ transmission_fidelity.py
@@ -7,6 +7,7 @@
 # 3) modification rate
 # 4) combination rate
 
+from cmath import e
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import cut
@@ -37,26 +38,44 @@ seed_traits = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
 # initialise culture group with two seed traits drawn at random
 culture_group = np.random.choice(seed_traits, 2, replace=False)
 
+# check intersection
+def has_intersection(a, b):
+        return not set(a).isdisjoint(b)
+    
 # run simulation
 culture_group_size = 2
-for i in range(1000):
+for i in range(100):
+    print(culture_group, culture_group_size, i, sep='\t')
     # draw random number between 0 and 1
     r = np.random.random()
-    # if r < rho1, new seed trait is introduced
-    if r < rho1:
+    # if r < rho1 or if there are no traits, new seed trait is introduced
+    if r < rho1 or culture_group_size == 0:
         # add new seed trait to culture group that doesn't exist in the group yet
         # seed traits not in group
         seed_traits_not_in_group = np.setdiff1d(seed_traits, culture_group)
+        # if all seed traits present repeat iteration
+        if len(seed_traits_not_in_group) == 0:
+            continue
         culture_group = np.append(culture_group, np.random.choice(seed_traits_not_in_group, 1, replace=False))
         culture_group_size += 1
-    # if r < rho1 + rho2, two of the cultural traits present are combined
+    # two of the cultural traits present are combined
     elif r < rho1 + rho2:
         # draw two random traits from culture group
         trait_1 = np.random.choice(culture_group, 1, replace=False)
-        remaining_traits = np.setdiff1d(culture_group, trait_1)
+        # remove letter 'm' from trait_1
+        trait_1_seed = trait_1[0].replace('m', '')
+        # check for traits in culture_group with no overlap in seed traits
+        remaining_traits = []
+        for trait in culture_group:
+            if not has_intersection(trait_1_seed, trait):
+                remaining_traits.append(trait)
+        # if there is nothing to combine with, repeat iteration
+        if remaining_traits == []:
+            i = i-1
+            continue
+        
         trait_2 = np.random.choice(remaining_traits, 1, replace=False)
-        # combine traits if not same seed trait
-        #if trait_1[0] != trait_2[0]:
+        # combine traits
         culture_group = np.append(culture_group, trait_1[0] + trait_2[0])
         culture_group_size += 1
     # if r < rho1 + rho2 + rho3, one of the cultural traits is modified
