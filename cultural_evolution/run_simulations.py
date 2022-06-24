086d918a7b162dbabe383cd27341980869e704ed
Martin Stoffel <martin.adam.stoffel@gmail.com>
2022-06-24 17:19:29 +0100

    standardised PCA

--- cultural_evolution/run_simulations.py
+++ cultural_evolution/run_simulations.py
@@ -30,25 +30,30 @@ for i in tqdm(range(len(all_pars))):
     cult_complex[i, :] = run_simulation(rho1 = all_pars[i, 0],
                                         rho2 = all_pars[i, 1],
                                         rho3 = all_pars[i, 2],
-                                        num_iter=100)
+                                        num_iter=1000)
 
 # save cult_complex as txt file
 np.savetxt('cult_complex.txt', cult_complex)
 
+# load cult_complex from txt file
+cult_complex = np.loadtxt('cultural_evolution/output/cult_complex.txt')
+# standardize cult_complex per column
+cult_complex_std = (cult_complex - cult_complex.mean(axis=0)) / cult_complex.std(axis=0)
 # run a PCA on all_combs to get a composite measure of cultural complexity
-pca = PCA(n_components=3)
-pca.fit(cult_complex)
-all_combs_pca = pca.transform(cult_complex)
+pca = PCA(n_components=6)
+pca.fit(cult_complex_std)
+all_combs_pca = pca.transform(cult_complex_std)
 # explained variance per pc in percent
 print(pca.explained_variance_ratio_)
 
 # combine all_pars, cult_complex and all_combs_pca into a dataframe
-df = pd.DataFrame(np.concatenate((all_pars, cult_complex, all_combs_pca), axis=1),
+df = pd.DataFrame(np.concatenate((all_pars, cult_complex_std, all_combs_pca), axis=1),
                     columns=['rho1', 'rho2', 'rho3', 'rho4', 'c1', 'c2', 'c3', 'c4', 'c5',
                              'c6', 'c7', 'c8', 'c9', 'c10', 'pc1', 'pc2', 'pc3'])
 # add grouping cult_complex to all_pars, every 10 rows is a new culture_group simulation
 df['culture_group'] = np.repeat(np.arange(0, len(all_pars), 10), 10)
-
+# reverse pc1
+df['pc1'] = -df['pc1']
 # multi-plot grid with boxplot for rho1, rho2, rho3, rho4 against pc1
 fig, ax = plt.subplots(2, 2, figsize=(10, 10))
 sns.boxplot(x = 'rho1', y='pc1', data=df, ax=ax[0, 0]).set(xlabel='novel invention rate')
@@ -58,7 +63,7 @@ sns.boxplot(x='rho4', y='pc1', data=df, ax=ax[1, 1]).set(xlabel='loss rate')
 plt.show()
 
 # save plot
-fig.savefig('cultural_evolution/figs/cult_evo.png')
+fig.savefig('cultural_evolution/figs/cult_evo2.png')
 
 
 
