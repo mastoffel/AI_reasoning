2697de498210131306ec78cc70962a25ec7ff151
Martin Stoffel <martin.adam.stoffel@gmail.com>
2022-07-07 15:09:28 +0100

    plot sim

---
+++ AI_evolution/evaluate_AI_simulation.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(here)
+library(ggplot2)
+source("theme_simple.R")
+
+# read csv with here
+df <- read_csv(here("AI_evolution", "output", "ai_complex_df.csv"))
+
+# mean over last 100 iterations
+df_plot <- df %>% 
+      mutate(sim_num = rep(rep(1:10, each = 500), nrow(.)/5000)) %>% 
+      group_by(sim_num, iter, judge, reason) %>% 
+      #slice_tail(n = 100) %>% 
+      summarise(across(c1:c10, mean)) 
+
+df_plot %>%
+      ggplot(aes(factor(reason), c10)) +
+            geom_boxplot() +
+            facet_wrap(~judge, nrow = 1)
+
+p1 <- df %>% 
+  ggplot(aes(iter, c10)) +
+  geom_line(size = 0.01, alpha = 0.3) +
+  theme_simple(axis_lines = TRUE, grid_lines = FALSE) +
+  facet_grid(judge ~ reason, 
+             labeller = labeller(.rows = label_both, .cols = label_both)) +
+  scale_x_continuous(breaks = c(100, 300, 500)) +
+  scale_y_continuous(breaks = c(1, 5, 10)) +
+  ylab("Mean utility over traits") +
+  xlab("Iteration")
+#p1
+
+ggsave("AI_evolution/figs/AI_evo_utility.jpg", width = 6, height = 5)
+
---
+++ AI_evolution/run_AI_simulation.py
@@ -0,0 +1,70 @@
+import itertools
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+from AI_evolution.evolve_AI import run_simulation    
+
+# suppress scientific notation in array
+np.set_printoptions(suppress=True)
+
+# combinations from 0.2 to 1 of rho1, rho2, rho3, judge, reason
+rho1_range=rho2_range=rho3_range=np.arange(2, 7, 2)/10  
+
+judge_range=reason_range=np.arange(2, 11, 2)/10  
+
+# create all combinations of rho1, rho2, rho3, rho4
+par_combs = np.array(list(itertools.product(rho1_range, rho2_range, 
+                                            rho3_range, judge_range,
+                                            reason_range)))  
+
+# get subset where rho1 is 0.4, rho2 is 0.3 and rho3 is 0.3
+# par_combs_sub = par_combs[(par_combs[:,0] == 0.4) & \
+#                                 (par_combs[:,1] == 0.3) & \
+#                                 (par_combs[:,2] == 0.3)]
+
+
+# get subset where rho1-rho3 sum up to 1
+par_combs_sub = par_combs[(par_combs[:,0] + par_combs[:,1] + par_combs[:,2] == 1)]
+
+# replicate each ai simulation 10 times
+all_pars = np.repeat(par_combs_sub, 10, axis=0)
+
+# add identifier from 1 to len(all_pars) to each row 
+all_pars = np.hstack((np.arange(1, len(all_pars)+1).reshape(-1,1), all_pars))
+
+
+# run simulation for each combination to get cultural complexities
+# 10 complexity measures are calculated
+num_iter = 500
+ai_complex = np.zeros(shape = (num_iter * len(all_pars), 10 + all_pars.shape[1] + 1))
+
+for i in tqdm(range(len(all_pars))):
+    
+    sim = run_simulation(rho1 = all_pars[i, 1],
+                                        rho2 = all_pars[i, 2],
+                                        rho3 = all_pars[i, 3],
+                                        judge = all_pars[i, 4],
+                                        reason = all_pars[i, 5],
+                                        num_iter=num_iter)
+    
+    # replicate all_pars[i] num_iter times
+    pars = np.tile(all_pars[i], num_iter).reshape(num_iter, all_pars.shape[1])
+    # add column for iteration number as integer
+    pars = np.concatenate((pars, np.arange(0, num_iter, 1).reshape(-1, 1)), axis=1)
+    # add pars to sim columnwise
+    sim = np.concatenate((pars, sim), axis=1)
+    # fill ai_complex with sim
+    ai_complex[i*num_iter:(i+1)*num_iter, :] = sim
+
+# ai_complex to dataframe
+ai_complex_df = pd.DataFrame(ai_complex, columns=['sim_id', 'rho1', 'rho2', 'rho3', 'judge', 'reason',
+                                                  'iter', 'c1', 'c2', 'c3', 'c4', 'c5',
+                                                  'c6', 'c7', 'c8', 'c9', 'c10'])
+
+# savee ai_complex_df to csv
+ai_complex_df.to_csv('AI_evolution/output/ai_complex_df.csv', index=False)
+
+
